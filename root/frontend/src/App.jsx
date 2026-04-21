6b7c8ea3f6174d68611d7d791b4a25de9b158b1c
bartahunor <barta.hunor.attila@diak.szbi-pg.hu>
2026-04-21 21:10:29 +0200

    footer was rebuilt in react

--- root/frontend/src/App.jsx
+++ root/frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        {/* oldalak majd ide kerülnek */}
-      </Routes>
+      <main className="min-h-screen">
+        <Routes>
+          {/* oldalak */}
+        </Routes>
+      </main>
+      <Footer />
     </BrowserRouter>
   )
 }
